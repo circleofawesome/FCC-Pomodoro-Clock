33447ae864acc024406813fdf4921f99b1bbac66
Samiul Anwar <samiul.anwar@gmail.com>
2016-07-11 20:33:33 -0400

    need to disable work buttons when break is happening and vice versa

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ function milToTime(){
 		playStatus+=1;
 		document.getElementById("work-pause-play").innerHTML="&#9658";
 		clearInterval(stopWatch);
-		//clockStatus="break";
 		breakClock();
 
 	}
@@ -39,8 +38,6 @@ var playStatus=0;
 //even #s mean its on play, odd means its on pause
 
 function countdownWork(){
-	/*stopWatch=setInterval(milToTime,1000);
-	document.getElementById("work-pause-play").innerHTML="&#10074 &#10074";*/
 	if(playStatus%2===0){
 		//even
 		playStatus+=1;
@@ -55,9 +52,7 @@ function countdownWork(){
 	}
 }
 
-function pause(){
-	clearInterval(stopWatch);
-}
+
 
 function plusWorkTime(){
 	clearInterval(stopWatch);
@@ -108,36 +103,93 @@ function breakClock(){
 	var breakTime=5000;
 	breakTime-=1000;
 
-	function milToTime(){
+	function milToTimeBreak(){
 		var mil=breakTime;
-		var initTime=time;
+		var initTime=breakTime;
 		mil/=1000;
 		var mins=0;
 		while(mil>59){
 				mil-=60;
 				mins+=1;
 			}
-		time-=1000;
+		breakTime-=1000;
 		if(mil<10){
 			console.log(mins.toString()+":0"+mil.toString());
-			document.getElementById("work-time-id").innerHTML=mins.toString()+":0"+mil.toString();
+			document.getElementById("break-time-id").innerHTML=mins.toString()+":0"+mil.toString();
 			
 		}
 		else{
 			console.log(mins.toString()+":"+mil.toString());
-			document.getElementById("work-time-id").innerHTML=mins.toString()+":"+mil.toString();
+			document.getElementById("break-time-id").innerHTML=mins.toString()+":"+mil.toString();
 		}
-		if(time<0){
+		if(breakTime<0){
 			//time=5000;
-			time=initTime;
-			console.log(time);
+			breakTime=initTime;
+			console.log(breakTime);
 			playStatus+=1;
-			document.getElementById("work-pause-play").innerHTML="&#9658";
+			document.getElementById("break-pause-play").innerHTML="&#9658";
 			clearInterval(stopWatch);
-			//clockStatus="break";
-			breakClock();
+			//at this point break ends, gonna need to run work again somehow here
+		}
+	}
+	var stopWatch;
+	var playStatus=0;
+	//even #s mean its on play, odd means its on pause
 
+	function countdownBreak(){
+		if(playStatus%2===0){
+			//even
+			playStatus+=1;
+			document.getElementById("break-pause-play").innerHTML="&#10074 &#10074";
+			stopWatch=setInterval(milToTimeBreak,1000);
+		}
+		else{
+			//odd
+			playStatus+=1;
+			document.getElementById("break-pause-play").innerHTML="&#9658";
+			clearInterval(stopWatch);
 		}
 	}
 
+	function plusBreakTime(){
+		clearInterval(stopWatch);
+		breakTime+=60000;
+		breakTime=remainderRemover(breakTime);
+		if(breakTime>3600000){
+			breakTime=3600000;
+		}
+		var updatedTime=milToTimePureBreak(breakTime);
+		document.getElementById("break-time-id").innerHTML=updatedTime;
+	}
+
+	function minusBreakTime(){
+		clearInterval(stopWatch);
+		breakTime-=60000;
+		breakTime=remainderRemover(breakTime);
+		if(breakTime<=0){
+			breakTime=0;
+		}
+		var updatedTime=milToTimePureBreak(breakTime);
+		document.getElementById("break-time-id").innerHTML=updatedTime;
+	}
+
+	function milToTimePureBreak(mil){
+		mil/=1000;
+		var mins=0;
+		while(mil>59){
+			mil-=60;
+			mins+=1;
+		}
+		if(mil<10){
+			return mins.toString()+":0"+mil.toString();
+		}
+		return mins.toString()+":"+mil.toString();
+	}
+
+	function remainderRemover(mil){
+		var seconds=mil%60000;
+		return mil-seconds;
+	}
+
+	countdownBreak();
 }
